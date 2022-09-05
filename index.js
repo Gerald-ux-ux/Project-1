92c041fe84b2455a850467706fb2229961e92332
gerald <you@example.com>
2022-09-05 15:23:15 +0300

    Creating Variables

---
+++ index.js
@@ -0,0 +1,4 @@
+let cat = document.getElementById('quote-tags')
+let quote = document.getElementById('quote-text')
+let nam = document.getElementById('quote-author')
+let quoteBtn = document.getElementById('gen-new-quote')
