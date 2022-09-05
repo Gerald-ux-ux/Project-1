ccb731ff7d0a22a7bc8207e8104e30e27d8b71ae
gerald <you@example.com>
2022-09-05 16:26:05 +0300

    Fetching the api

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ let quoteBtn = document.getElementById('gen-new-quote')
 
 const url = "https://type.fit/api/quotes";
 
-function generateQuote()
+function generateQuote() {
+
+    return fetch(url)
+        .then(resp =>resp.json())
+       .then(quotes => {
+           let len = quotes.length;
+           let i = Math.floor(Math.random() * len)
+           let qt = quotes[i];
+           quote.textContent = qt.text;
+           nam.textContent = ` -- ${qt.author}`;
+
+        })
 
