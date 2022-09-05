576841318ae1956e705fc21da90346d8eb7467a3
gerald <you@example.com>
2022-09-05 15:24:27 +0300

    Fetching

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let cat = document.getElementById('quote-tags')
 let quote = document.getElementById('quote-text')
 let nam = document.getElementById('quote-author')
 let quoteBtn = document.getElementById('gen-new-quote')
+
+const url = "https://type.fit/api/quotes";
